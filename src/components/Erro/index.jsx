88597c35ac25a5acab2f9786066385ccef644a60
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-11-09 09:37:08 -0300

    mensagem de erro ao nao localizar uf

---
+++ src/components/Erro/index.jsx
@@ -0,0 +1,11 @@
+import "./style.scss"
+
+
+export default function Erro(){
+
+    return(
+        <div className="conteudo">
+            <div><h2>Erro, população não localizada ou inexistente</h2></div>
+        </div>
+    )
+}
